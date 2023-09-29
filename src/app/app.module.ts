3e770c4e3b4db50aa759dc33fd96a04453cbf177
Andre Jorge Raposo <andre2308@gmail.com>
2023-09-28 22:34:01 -0300

    19.Component footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,13 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { FooterComponent } from './components/footer/footer.component';
+
+/*https://material.angular.io/components/toolbar/api*/
+/*https://material.angular.io/*/
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
