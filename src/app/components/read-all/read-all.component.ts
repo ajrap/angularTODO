e6e4ade71148c0b62e69fc8c91515c6803a61972
Andre Jorge Raposo <andre2308@gmail.com>
2023-09-29 23:09:37 -0300

    21.Componente read-all - Parte 2

--- src/app/components/read-all/read-all.component.ts
+++ src/app/components/read-all/read-all.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Todo } from "src/app/models/todo";
 
 @Component({
-  selector: 'app-read-all',
-  templateUrl: './read-all.component.html',
-  styleUrls: ['./read-all.component.css']
+  selector: "app-read-all",
+  templateUrl: "./read-all.component.html",
+  styleUrls: ["./read-all.component.css"],
 })
-export class ReadAllComponent {
+export class ReadAllComponent implements OnInit {
+  list: Todo[] = [
+    {
+      titulo: "Teste",
+      dataParaFinalizar: new Date(),
+      finalizado: false,
+    },
+    {
+      titulo: "Teste 2",
+      dataParaFinalizar: new Date(),
+      finalizado: false,
+    },
+    {
+      titulo: "Teste 3",
+      dataParaFinalizar: new Date(),
+      finalizado: false,
+    }
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
